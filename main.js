0b8dbb5f50ba12663ffd22ad64669d01be173adc
Mico de los Santos <mico.dlsantos@gmail.com>
2015-08-10 16:55:25 +0800

    Added solution to Logger

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var hv = require('./highestValue.js').highestValue;
 var hl = require('./highLow.js').highLow;
 var ct = require('./currentTime.js').currentTime;
 var fm = require('./flattenMe.js').flattenMe;
+var l = require('./logger.js').logger;
 var p = require('./print.js').print;
 
 console.log('1. ' + nr(123));
@@ -23,5 +24,10 @@ console.log('9. ' + hl([4, 15, 13, 20, 20]));
 console.log('10. ');
 // ct();
 console.log('11. ' + fm([1, [2, [3, 4], [[5]], 6]]));
+console.log('14. ');
+var info = l('INFO:')
+info('this is an info message')
+var warn = l('WARN:')
+warn('this is a warning message', 'with more info')
 console.log('15. ');
 p();
