5e086da1a86b29308c8a3cef13ad0063869e1e4d
Mico de los Santos <mico.dlsantos@gmail.com>
2015-08-11 09:43:45 +0800

    Fixed spacing

--- logger.js
+++ logger.js
@@ -1,9 +1,9 @@
 function logger(namespace) {
-  return function() {
-    var args = Array.prototype.slice.call(arguments);
-    args.unshift(namespace);
-    console.log.apply(this, args);
-  }
+    return function() {
+        var args = Array.prototype.slice.call(arguments);
+        args.unshift(namespace);
+        console.log.apply(this, args);
+    }
 }
 
 module.exports.logger = logger
